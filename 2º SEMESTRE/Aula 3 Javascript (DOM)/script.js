24fd3d575f3ef9d271577d9b119299b205d4e3d6
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-08-04 12:21:49 -0300

    Add initial HTML, CSS, and JavaScript files for Aula 3 (DOM)

---
+++ 2º SEMESTRE/Aula 3 Javascript (DOM)/script.js
@@ -0,0 +1,10 @@
+const demo = document.getElementById("demo");
+
+demo.innerHTML= `<ol>
+                <li>
+                Eu sou.
+                </li>
+                <li>
+                Eu sou javascript.
+                </li>
+                </ol>`;
