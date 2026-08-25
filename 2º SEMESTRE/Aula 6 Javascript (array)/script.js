482683f2b5002185524ce5b3912791104d748b96
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-08-25 12:44:37 -0300

    Avançando em Arrays, método reduce, novos laços de repetição (forEach), funções encadeadas.

--- 2º SEMESTRE/Aula 6 Javascript (array)/script.js
+++ 2º SEMESTRE/Aula 6 Javascript (array)/script.js
@@ -14,6 +14,31 @@ busca = frutas.indexOf("Maçã");
 
 console.log(busca);
 
+//Três formas de percorrer todos os elementos do Array
+
 for (let i = 0; i < frutas.length; i++){
     console.log(frutas[i]);
 }
+
+let i = 0;
+while (i<frutas.length){
+    console.log(frutas[i]);
+    i++;
+}
+
+frutas.forEach(frutas => {
+    console.log(frutas); //não pega elementos indefinidos
+})
+
+let number = new Array (1,2,3,4,5,6,7);
+console.log(number);
+
+number = number.map(number => number * 2);
+console.log(number);
+
+//Método que percorre todos os Arrays e efetua a soma de todos
+console.log(number.reduce((soma, number) => soma + number));
+
+const preco = [10.99,20.99,38.99];
+
+preco.map(preco => preco * 1.1).forEach(preco => console.log(preco));
