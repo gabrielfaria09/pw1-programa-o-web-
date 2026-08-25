dbd10b1e52563035ab0e889f69b48b8f0ed8c01a
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-08-25 08:00:05 -0300

    Redirect para WhastApp

--- 2º SEMESTRE/Aula 5 Javascript (FORM)/script.js
+++ 2º SEMESTRE/Aula 5 Javascript (FORM)/script.js
@@ -1,6 +1,7 @@
 function login(){
     const user = document.getElementById("fuser").value;
     const password = document.getElementById("fpassword").value;
+    const tipo = document.getElementById("tipo").value;
 
     if( user === "adm" && password === "123"){
         alert("Senha correta!");
@@ -8,7 +9,6 @@ function login(){
         alert("Senha incorreta!");
     }
 
+    window.open(`https://wa.me/5519988570715?text=${user} deseja entrar como ${tipo}`, '_blank');
+
 }
-
-
-login();
