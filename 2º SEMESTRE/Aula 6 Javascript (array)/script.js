6744164474b4ba85f41c5be34175cfeb0df7a223
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-08-25 08:21:10 -0300

    Aula de Array (introdução)

---
+++ 2º SEMESTRE/Aula 6 Javascript (array)/script.js
@@ -0,0 +1,19 @@
+const frutas = ["Maçã", "Laranja", "Abacaxi"];
+
+tamanho = frutas.length;
+
+console.log(tamanho);
+console.log(frutas);
+
+frutas.splice(1, 0, "Uva");
+
+console.log(frutas);
+console.log(frutas[0]);
+
+busca = frutas.indexOf("Maçã");
+
+console.log(busca);
+
+for (let i = 0; i < 4; i++){
+    console.log(frutas[i]);
+}
