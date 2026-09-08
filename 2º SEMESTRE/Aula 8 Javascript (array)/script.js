ab2ca0769fe1dbcf77654a8ef772e536ff924eb9
gabrielfaria09 <gabrielfaria0905@gmail.com>
2026-09-08 12:36:02 -0300

    Remoção de conteúdo obsoleto do arquivo script principal.

--- 2º SEMESTRE/Aula 8 Javascript (array)/script.js
+++ 2º SEMESTRE/Aula 8 Javascript (array)/script.js
@@ -8,24 +8,6 @@ const produtos = [
 const catalogo = document.getElementById('catalogo');
 const pesquisa = document.getElementById('pesquisa');
 
-/*produtos.forEach(produto => {
-    const [nome,armazenamento, memória, preço] = produto;
-    //const paragrafo = document.createElement('p');
-    const divisao = document.createElement('div');
-    divisao.className = "card";
-
-    divisao.innerHTML = `<h4>${nome}</h4>
-                            <p>${armazenamento}</p>
-                            <p>${memória}</p>
-                            <p>${preço}</p>`;
-    
-    catalogo.append(divisao);
-});
-
-produtos.filter(([Nome, armazenamento, memória, preço]) => {
-    return Nome.toLowerCase().includes(pesquisa.toLowerCase());
-})*/
-
 function filtrar(){
     const valor = pesquisa.value.toLowerCase();
 
